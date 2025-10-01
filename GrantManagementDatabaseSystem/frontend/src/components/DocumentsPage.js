bff59dcc870715a18d7b5d02d83801bb9fdcc253
ayatipandey <ayatipandey03@gmail.com>
2025-10-01 15:15:06 -0400

    CSS Updates for Documents page

--- GrantManagementDatabaseSystem/frontend/src/components/DocumentsPage.js
+++ GrantManagementDatabaseSystem/frontend/src/components/DocumentsPage.js
@@ -1,15 +1,49 @@
 import { useState } from "react";
+import { Link } from "react-router-dom"; 
 import "./DocumentsPage.css";
 
 function DocumentsPage() {
+  const mockDocuments = [
+    {
+      grantId: "G001",
+      name: "Food Bank Research",
+      type: "Research Proposal",
+      status: "Completed",
+      date: "01-01-2025",
+    },
+    {
+      grantId: "G002",
+      name: "Food and Food Budget Plan",
+      type: "Budget Plan",
+      status: "In-review",
+      date: "02-02-2025",
+    },
+    {
+      grantId: "G003",
+      name: "Foodie's Place Grant Proposal",
+      type: "Grant Proposal",
+      status: "In-progress",
+      date: "03-03-2025",
+    },
+  ];
+
   return (
+     <div className="documents-page">
+      
+      <header className="documents-headerbar">
+        <div className="logo">Epic-cure-logo</div>
+        <nav className="nav-links">
+          <Link to="/home">Home</Link>
+          <Link to="/grants">Grants</Link>
+          <Link to="/home">Logout</Link>
+        </nav>
+      </header>
     <div className="documents-container">
 
       <div className="documents-header">
         <h1>Documents</h1>
       </div>
 
-    
       <div className="documents-controls">
         <input
           type="text"
@@ -18,21 +52,35 @@ function DocumentsPage() {
         />
 
         <button className="documents-button">Sort By</button>
-        <button className="documents-button">Upload Document</button>
+         <Link to="/AddDocuments">
+          <button className="documents-button">Upload Document</button>
+         </Link>
       </div>
 
       <table className="documents-table">
-        <thead>
-          <tr>
-            <th>Grant ID</th>
-            <th>Document Name</th>
-            <th>Document Type</th>
-            <th>Document Status</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-      </table>
+          <thead>
+            <tr>
+              <th>Grant ID</th>
+              <th>Document Name</th>
+              <th>Document Type</th>
+              <th>Document Status</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {mockDocuments.map((doc) => (
+              <tr key={doc.grantId}>
+                <td>{doc.grantId}</td>
+                <td>{doc.name}</td>
+                <td>{doc.type}</td>
+                <td>{doc.status}</td>
+                <td>{doc.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       
+    </div>
     </div>
   );
 }
